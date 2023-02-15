5e333997d0892b3b320268bf3df02d8d6e6320b5
Anders Kolstad <anders.kolstad@nina.no>
2023-02-15 15:27:03 +0100

    halfway to update data

--- dataRAW.R
+++ dataRAW.R
@@ -4,9 +4,10 @@ library(units)
 library(stringr)
 library(units)
 
-# This script take the file Natur_Naturtyper_NiN_norge_med_svalbard_25833.gdb, stored in 
-# "P:/41001581_egenutvikling_anders_kolstad/data/" and processes it for the shiny app.
+# This script take the file downloaded from geoNorge and processes it for the shiny app.
 # Replace the file and rerun the script when neccessary.
+
+# UPDATE 15.02.23: Updated the script to include year 2022. Several of the columns had new names!
 
 
 varList <- c("kartleggingsår", 
@@ -17,22 +18,28 @@ varList <- c("kartleggingsår",
                        "usikkerhet", 
                        "hovedøkosystem", 
                        "oppdragstaker", 
-                       "uk_nærtruet",
-                       "uk_sentraløkosystemfunksjon",
-                       "uk_spesieltdårligkartlagt",
-                       "uk_truet")
+                       "kriterium_nærTruet",
+                       "kriterium_sentralØkosystemFunksjon",
+                       "kriterium_spesieltDårligKartlagt",
+                       "kriterium_truet")
 
 
 
 # path to data set
 dir <- substr(getwd(), 1,2)
+
+#path_old <- ifelse(dir == "C:", 
+#                "P:/41001581_egenutvikling_anders_kolstad/data/",
+#                "/data/Egenutvikling/41001581_egenutvikling_anders_kolstad/data/")
+
 path <- ifelse(dir == "C:", 
-                "P:/41001581_egenutvikling_anders_kolstad/data/",
-                "/data/Egenutvikling/41001581_egenutvikling_anders_kolstad/data/")
+               "R:/GeoSpatialData/Habitats_biotopes/Norway_Miljodirektoratet_Naturtyper_nin/Original/Naturtyper_nin_0000_norge_25833_FILEGDB/Naturtyper_nin_0000_norge_25833_FILEGDB.gdb",
+               "/data/R/GeoSpatialData/Habitats_biotopes/Norway_Miljodirektoratet_Naturtyper_nin/Original/Naturtyper_nin_0000_norge_25833_FILEGDB/Naturtyper_nin_0000_norge_25833_FILEGDB.gdb")
 
 
 # Read data ----------------------
-dat <- sf::read_sf(paste0(path, "Natur_Naturtyper_NiN_norge_med_svalbard_25833.gdb"))
+#dat <- sf::read_sf(paste0(path, "Natur_Naturtyper_NiN_norge_med_svalbard_25833.gdb"))
+dat <- sf::read_sf(path)
 
 # Get county delimination
 path_county <- ifelse(dir == "C:", 
@@ -54,13 +61,14 @@ counties$fylke <- counties_names$navn[match(counties$objid, counties_names$fylke
 #  tm_polygons(col = "fylke")
 
 # Fix some naming errors
+unique(counties$fylke)
 counties <- counties %>%
   mutate(fylke =  recode(fylke,
                        "MÃ¸re og Romsdal" = "Møre og Romsdal",
                        "TrÃ¸ndelag" = "Trøndelag"))
 
 
-# Finding and removing 'old' nature types that were not mapped in 2021 ---------------
+# Finding and removing 'old' nature types that were not mapped in 2022 ---------------
 # list all types
 keepers_pre <- unique(dat$naturtype)
 #Extract the year when these were mapped
@@ -74,7 +82,7 @@ keepers_df <- data.frame(
   "Year"        = years
 )
 # find those mapped in 2021
-keepers <- keepers_df$Nature_type[grepl("2021" , keepers_df$Year)]
+keepers <- keepers_df$Nature_type[grepl("2022" , keepers_df$Year)]
 # and cut the rest
 dat <- dat[dat$naturtype %in% keepers,]
 
@@ -83,40 +91,41 @@ dat <- dat[dat$naturtype %in% keepers,]
 dat_counties <- st_intersection(dat, counties)
 # Check for boundary issues
 unique(st_geometry_type(dat_counties)) # no lines introduced
-nrow(dat)-nrow(dat_counties)  # 30 new polygons means 60 polygons were split between counties
+nrow(dat)-nrow(dat_counties)  # 47 new polygons means 94 polygons were split between counties
 # find those that are duplicated
-dups <- dat_counties$identifikasjon_lokalid[duplicated(dat_counties$identifikasjon_lokalid)]
-dups2 <- dat_counties[dat_counties$identifikasjon_lokalid %in% dups,]
+dups <- dat_counties$identifikasjon_lokalId[duplicated(dat_counties$identifikasjon_lokalId)]
+dups2 <- dat_counties[dat_counties$identifikasjon_lokalId %in% dups,]
 # Assign these to the county where they are biggest
 # - force non-scientific nmbers
 options("scipen"=100, "digits"=4)
 # calculate area
 dups3 <- dups2 %>%
-  mutate(area = units::drop_units(st_area(dups2))/1000, .after =  identifikasjon_lokalid)
+  mutate(area = units::drop_units(st_area(dups2))/1000, .after =  identifikasjon_lokalId)
 # Arrange by area and chose the one (the row) with the biggest area. Thta row will contain the 'fylke' that we want to assign this locality to.
 dups4 <- dups3 %>%
-  group_by(identifikasjon_lokalid) %>%
+  group_by(identifikasjon_lokalId) %>%
   arrange(desc(area), .by_group=T) %>%
   slice_head(n = 1)
 # Remove all duplicates from the data set
-'%!in%' <- Negate('%in%')
+#'%!in%' <- Negate('%in%')
 dat_counties_filtered <- dat_counties %>%
-  filter(identifikasjon_lokalid %!in% dups4$identifikasjon_lokalid )
+  filter(!identifikasjon_lokalId %in% dups4$identifikasjon_lokalId )
 # Find the duplicatd localities in the data set from before the intersection 
 to_add <- dat %>%
-  filter(identifikasjon_lokalid %in% dups4$identifikasjon_lokalid)
+  filter(identifikasjon_lokalId %in% dups4$identifikasjon_lokalId)
 # Attach fylke name to those
-to_add$fylke <- dups4$fylke[match(to_add$identifikasjon_lokalid, dups4$identifikasjon_lokalid)]
+to_add$fylke <- dups4$fylke[match(to_add$identifikasjon_lokalId, dups4$identifikasjon_lokalId)]
 # and bind with the rest of the data
 dat_counties_filtered_added <- bind_rows(dat_counties_filtered,
                                          to_add)
   
-#View(dat_counties_filtered_added[dat_counties_filtered_added$identifikasjon_lokalid %in% dups4$identifikasjon_lokalid, ])   #OK
+#View(dat_counties_filtered_added[dat_counties_filtered_added$identifikasjon_lokalId %in% dups4$identifikasjon_lokalId, ])   #OK
 
 
-# rename
+# rename and clean
 dat <- dat_counties_filtered_added
-
+rm(counties, counties_names, dat_counties, dat_counties_filtered, dat_counties_filtered_added,
+   dups, dups2, dups3, dups4, keepers_df, to_add)
 
 # Combine fylke into regions --------------------------------------
 dat <- dat %>%
@@ -127,54 +136,58 @@ dat <- dat %>%
                    fylke %in% c("Rogaland", "Vestland") ~ "Vestlandet",
                    fylke %in% c("Møre og Romsdal", "Trøndelag") ~ "Midt-Norge"))
 
+#table(dat$region)
+
 # Calculate area
 dat$km2 <- drop_units(st_area(dat))/1000
 
 
 
 # Some recoding --------------------------------------------
+names(dat)
+
 dat2 <- dat %>%
   as.data.frame() %>%
   select(-SHAPE) %>%
-  mutate(uk_ingenstatus = str_replace_na(uk_ingenstatus, "Nei")) %>%
-  mutate(uk_ingenstatus = str_replace(uk_ingenstatus, "1", "Ja")) %>%
+  mutate(kriterium_ingenStatus = str_replace_na(kriterium_ingenStatus, "Nei")) %>%
+  mutate(kriterium_ingenStatus = str_replace(kriterium_ingenStatus, "1", "Ja")) %>%
   
-  mutate(uk_nærtruet = str_replace_na(uk_nærtruet, "Nei")) %>%
-  mutate(uk_nærtruet = str_replace(uk_nærtruet, "1", "Ja")) %>%
+  mutate(kriterium_nærTruet = str_replace_na(kriterium_nærTruet, "Nei")) %>%
+  mutate(kriterium_nærTruet = str_replace(kriterium_nærTruet, "1", "Ja")) %>%
   
-  mutate(uk_sentraløkosystemfunksjon = str_replace_na(uk_sentraløkosystemfunksjon, "Nei")) %>%
-  mutate(uk_sentraløkosystemfunksjon = str_replace(uk_sentraløkosystemfunksjon, "1", "Ja")) %>%
+  mutate(kriterium_sentralØkosystemFunksjon = str_replace_na(kriterium_sentralØkosystemFunksjon, "Nei")) %>%
+  mutate(kriterium_sentralØkosystemFunksjon = str_replace(kriterium_sentralØkosystemFunksjon, "1", "Ja")) %>%
   
-  mutate(uk_spesieltdårligkartlagt = str_replace_na(uk_spesieltdårligkartlagt, "Nei")) %>%
-  mutate(uk_spesieltdårligkartlagt = str_replace(uk_spesieltdårligkartlagt, "1", "Ja")) %>%
+  mutate(kriterium_spesieltDårligKartlagt = str_replace_na(kriterium_spesieltDårligKartlagt, "Nei")) %>%
+  mutate(kriterium_spesieltDårligKartlagt = str_replace(kriterium_spesieltDårligKartlagt, "1", "Ja")) %>%
   
-  mutate(uk_truet = str_replace_na(uk_truet, "Nei")) %>%
-  mutate(uk_truet = str_replace(uk_truet, "1", "Ja")) %>%
+  mutate(kriterium_truet = str_replace_na(kriterium_truet, "Nei")) %>%
+  mutate(kriterium_truet = str_replace(kriterium_truet, "1", "Ja")) %>%
 
   mutate(across(all_of(varList), ~na_if(., ''))) %>%
   
   mutate(tilstand =  recode(tilstand, 
-                            "Svært redusert" = "1 - Svært redusert",
-                            "Dårlig" = "2 - Dårlig",
-                            "Moderat" = "3 - Moderat",
-                            "God" = "4 - God")) %>%
+                            "sværtRedusert" = "1 - Svært redusert",
+                            "dårlig" = "2 - Dårlig",
+                            "moderat" = "3 - Moderat",
+                            "god" = "4 - God")) %>%
   mutate(naturmangfold =  recode(naturmangfold, 
-                            "Lite" = "1 - Lite",
-                            'Moderat' = "2 - Moderat",
-                            "Stort" = "3 - Stort")) %>%
+                            "lite" = "1 - Lite",
+                            'moderat' = "2 - Moderat",
+                            "stort" = "3 - Stort")) %>%
   mutate(lokalitetskvalitet =  recode(lokalitetskvalitet, 
-                                 "Svært lav kvalitet" = "1 - Svært lav kvalitet",
-                                 'Lav kvalitet' = "2 - Lav kvalitet",
-                                 "Moderat kvalitet" = "3 - Moderat kvalitet",
-                                 "Høy kvalitet" = "4 - Høy kvalitet",
-                                 "Svært høy kvalitet" = "5 - Svært høy kvalitet"))
+                                 "sværtLavKvalitet" = "1 - Svært lav kvalitet",
+                                 'lavKvalitet' = "2 - Lav kvalitet",
+                                 "moderatKvalitet" = "3 - Moderat kvalitet",
+                                 "høyKvalitet" = "4 - Høy kvalitet",
+                                 "sværtHøyKvalitet" = "5 - Svært høy kvalitet"))
   
 
 
 # Melted data set  -----------------------------------------
 # (one row for each nin-variable within each locality)
-dat2_long <- tidyr::separate_rows(dat2, ninbeskrivelsesvariabler, sep=",") %>%
-  separate(col = ninbeskrivelsesvariabler,
+dat2_long <- tidyr::separate_rows(dat2, ninBeskrivelsesvariable, sep=",") %>%
+  separate(col = ninBeskrivelsesvariable,
            into = c("NiN_variable_code", "NiN_variable_value"),
            sep = "_",
            remove=F) %>%
@@ -182,6 +195,10 @@ dat2_long <- tidyr::separate_rows(dat2, ninbeskrivelsesvariabler, sep=",") %>%
   filter(!str_detect(NiN_variable_code, "LKM")) %>%
   group_by(naturtype, NiN_variable_code, NiN_variable_value) %>%
   filter(n() > 2) %>% ungroup()
+# The 'expected two pieces' warning is fine to ignore
+
+
+# STOPPED HERE 15.02  --------------------
 
 
 # Remove nin-variables not recorded in 2021 ---------------
@@ -228,7 +245,7 @@ length(unique(dat2_long_2$NiN_variable_code))-length(unique(dat2_long_3$NiN_vari
 
 
 # Check for other weird NiN variable names ----------------------------
-# There are a lot of errors in the column ninbeskrivelsesvariabler
+# There are a lot of errors in the column ninBeskrivelsesvariable
 View(table(dat2_long_3$NiN_variable_code))
 
 # This looks better.
